9277cced286a76a989d1373dc83d4b261ec907c5
devsoopark <sparkm0128@gmail.com>
2019-07-30 17:52:47 +0900

    ENH: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'js/app': ['./src/App.tsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new ForkTsCheckerWebpackPlugin({ silent: true }),
+  ],
+};
