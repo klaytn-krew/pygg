f4d1e9d866841882dd9bf73f8ea5d5c498c9ffea
RyuChain <ryugihyeok@gmail.com>
2019-08-13 11:15:50 +0900

    ENH: colors 추가

---
+++ src/constants/colors.ts
@@ -0,0 +1,41 @@
+const COLOR = {
+  blue: "#0051C7",
+  skyLightBlue: "#F7FBFF",
+  skyDarkBlue: "#E6F1FC",
+  lightBlue: "#0062DF",
+  darkBlue: "#093687",
+  red: "#D60000",
+  darkRed: "#FFEBF0",
+  lightRed: "#FFF7F9",
+  lightGrey: "#F4F5F8",
+  darkGrey: "#666666",
+  white: "#FFFFFF",
+  lightWhite: "#E9ECF1",
+  black: "#2B2B2B",
+  lineLightGrey: "#E9E9E9",
+  silverGrey: "#9EA0A4",
+  sellBlue: "#2568BA",
+  buyRed: "#E7586F"
+};
+
+export const THEME = {
+  palette: {
+    primary: COLOR.black,
+    secondary: COLOR.lightBlue,
+    up: COLOR.red,
+    down: COLOR.blue,
+    backgroundPrimary: COLOR.lightWhite,
+    backgroundSecondary: COLOR.lightGrey,
+    menuHeader: COLOR.silverGrey,
+    light: COLOR.white,
+    dark: COLOR.black,
+    info: COLOR.darkGrey,
+    line: COLOR.lineLightGrey,
+    buy: COLOR.buyRed,
+    sell: COLOR.sellBlue,
+    backgroundLightSell: COLOR.skyLightBlue,
+    backgroundDarkSell: COLOR.skyDarkBlue,
+    backgroundLightBuy: COLOR.lightRed,
+    backgroundDarkBuy: COLOR.darkRed
+  }
+};
