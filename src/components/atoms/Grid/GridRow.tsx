1b760a3e50906285850777b01f630c75b5d18dab
devsoopark <sparkm0128@gmail.com>
2019-08-20 19:42:00 +0900

    ENH: add GridRow atom

---
+++ src/components/atoms/Grid/GridRow.tsx
@@ -0,0 +1,52 @@
+import React from "react"
+import styled from "styled-components";
+import { IComponentProps } from "../../../constants/types";
+import {
+  IInnerType,
+  IOuterType,
+  IBorderType,
+  innerStyling,
+  outerStyling,
+  borderStyling,
+  contentAlignStyling,
+  IContentAlignType
+} from "../../../constants/styles";
+
+interface IGridRowProps extends IComponentProps {
+  _inner?: IInnerType;
+  _outer?: IOuterType;
+  _border?: IBorderType;
+  _contentAlign?: IContentAlignType;
+  _background_color?: string;
+}
+
+const SemanticStyledGridRow =  ({ className, ...props }: IGridRowProps) => (
+  <div className={className} {...props}>
+    {props.children}
+  </div>
+);
+
+const StyledGridRow = styled(SemanticStyledGridRow)`
+  ${props => props._inner && innerStyling(props._inner)}
+  ${props => props._outer && outerStyling(props._outer)}
+  ${props => props._border && borderStyling(props._border)}
+  ${props => props._contentAlign && contentAlignStyling(props._contentAlign)}
+  background-color: ${props => props._background_color} !important;
+  display: flex;
+`;
+
+export const SemanticGridRow = (props: IGridRowProps) => {
+  return (
+    <StyledGridRow
+      _inner={props._inner}
+      _outer={props._outer}
+      _border={props._border}
+      _background_color={props._background_color}
+      _contentAlign={props._contentAlign}
+    >
+      {props.children}
+    </StyledGridRow>
+  );
+};
+
+export type TGridRow = ReturnType<typeof SemanticGridRow>
