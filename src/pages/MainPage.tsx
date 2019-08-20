092e6b67700ae14f95107d8a1e7015a3028da34a
devsoopark <sparkm0128@gmail.com>
2019-08-20 19:44:02 +0900

    ENH: test Grid on MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,20 +1,36 @@
 import React from "react"
 
 import { Table, TableHeader, TableHeaderCell, TableBody, TableBodyCell } from "../components/atoms/Table"
+import { Grid, GridRow, GridColumn } from "../components/atoms/Grid";
 
 const MainPage = () => (
-  <Table _border="square">
-    <TableHeader>
-      <TableHeaderCell>
-        TableHeader
-      </TableHeaderCell>
-    </TableHeader>
-    <TableBody>
-      <TableBodyCell>
-        TableBody
-      </TableBodyCell>
-    </TableBody>
-  </Table>
+  // <Table _border="square">
+  //   <TableHeader>
+  //     <TableHeaderCell>
+  //       TableHeader
+  //     </TableHeaderCell>
+  //   </TableHeader>
+  //   <TableBody>
+  //     <TableBodyCell>
+  //       TableBody
+  //     </TableBodyCell>
+  //   </TableBody>
+  // </Table>
+  <Grid>
+    <GridRow _contentAlign="center">
+      <GridColumn _width={10}>
+        1
+      </GridColumn>
+      <GridColumn _width={5}>
+        1
+      </GridColumn>
+    </GridRow>
+    <GridRow>
+      <GridColumn>
+        2
+      </GridColumn>
+    </GridRow>
+  </Grid>
 );
 
 export default MainPage;
