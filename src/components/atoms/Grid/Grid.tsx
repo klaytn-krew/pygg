27c0c536d94c94dc1112c97e3b62fa7812f66188
devsoopark <sparkm0128@gmail.com>
2019-08-20 19:42:09 +0900

    ENH: add Grid atom

---
+++ src/components/atoms/Grid/Grid.tsx
@@ -0,0 +1,54 @@
+import React from "react"
+import styled from "styled-components";
+import { IComponentProps } from "../../../constants/types";
+import {
+  IInnerType,
+  IOuterType,
+  IBorderType,
+  innerStyling,
+  outerStyling,
+  borderStyling,
+  contentAlignStyling,
+  IContentAlignType
+} from "../../../constants/styles";
+import { TGridColumn } from "./GridColumn";
+import { TGridRow } from "./GridRow";
+
+interface IGridProps {
+  children?: TGridRow | TGridRow[] | TGridColumn | TGridColumn[];
+  className?: string;
+  _inner?: IInnerType;
+  _outer?: IOuterType;
+  _border?: IBorderType;
+  _contentAlign?: IContentAlignType;
+  _background_color?: string;
+}
+
+const SemanticStyledGrid =  ({ className, ...props }: IGridProps) => (
+  <div className={className} {...props}>
+    {props.children}
+  </div>
+);
+
+const StyledGrid = styled(SemanticStyledGrid)`
+  ${props => props._inner && innerStyling(props._inner)}
+  ${props => props._outer && outerStyling(props._outer)}
+  ${props => props._border && borderStyling(props._border)}
+  ${props => props._contentAlign && contentAlignStyling(props._contentAlign)}
+
+  background-color: ${props => props._background_color} !important;
+`;
+
+export const SemanticGrid = (props: IGridProps) => {
+  return (
+    <StyledGrid
+      _inner={props._inner}
+      _outer={props._outer}
+      _border={props._border}
+      _background_color={props._background_color}
+      _contentAlign={props._contentAlign}
+    >
+      {props.children}
+    </StyledGrid>
+  );
+};
---
+++ src/components/atoms/Grid/index.tsx
@@ -0,0 +1,5 @@
+import { SemanticGrid as Grid } from "./Grid";
+import { SemanticGridColumn as GridColumn } from "./GridColumn";
+import { SemanticGridRow as GridRow } from "./GridRow";
+
+export { Grid, GridColumn, GridRow };
